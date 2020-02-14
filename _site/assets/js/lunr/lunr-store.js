a47df85072ae05eef8c943ea1a9edb00329e97d3
yuksangeun <tkddms150@naver.com>
2020-02-14 17:08:28 +0900

    bfs algorithm post 작성

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,10 +4,10 @@ var store = [{
         "tags": ["BFS","DFS","graph"],
         "url": "http://localhost:4000/data%20structure/graph/",
         "teaser":null},{
-        "title": "Bfs",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/bfs/",
+        "title": "BFS Algorithm",
+        "excerpt":"BFS(Breadth First Search) - 너비 우선 탐색 탐색 모든 정점들을 특정 순서에 따라 방문하는 알고리즘 그래프의 구조를 알 수 있다. (bfs, dfs 알고리즘을 이용) BFS 시작점에 가까운 정점부터 순서대로 방문(모든 곳을 공평하게 조금씩 본다.) 즉 시작점을 0단계, 그 다음부터 1, 2, ..k..단계라고 할 때, k단계에 방문하는 정점들을 시작점으로부터 최단거리가 k라고...","categories": ["Algorithm"],
+        "tags": ["BFS","graph"],
+        "url": "http://localhost:4000/algorithm/bfs/",
         "teaser":null},{
         "title": "DFS Algorithm",
         "excerpt":"DFS(Depth First Search) - 깊이 우선 탐색 탐색 모든 정점들을 특정 순서에 따라 방문하는 알고리즘 그래프의 구조를 알 수 있다. (bfs, dfs 알고리즘을 이용) DFS는 그래프의 모든 정점을 발견하는 가장 단순하고 고전적인 방법 DFS 현재 정점에서 더 이상 다른 정점으로 이동할 수 없는 상태에 도달할 때까지 탐색 후 이전 정점으로...","categories": ["Algorithm"],
