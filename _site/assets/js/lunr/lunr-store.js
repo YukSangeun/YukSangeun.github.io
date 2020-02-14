97637d215cedbf59d2b585cec1c40a7d0f16fcec
yuksangeun <tkddms150@naver.com>
2020-02-14 16:17:17 +0900

    dfs post 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,16 @@
 var store = [{
         "title": "Graph",
-        "excerpt":"1. 그래프 설명 현실세계에서는 도로망, 지인관계, 링크관계 등의 연결고리들을 표현할 수 있다. 정의: 정점(vertex)과 간선(edge)의 집합 G(V,E) 정점: 노드(node)라고도 부른다. 간선: 유형에 따라 나뉜다. 두 정점을 이어주는 간선 자기자신을 이어주는 간선 방향이 있는 간선/ 없는 간선 - 방향이 있는 간선의 경우 사이클 발생 가능 가중치가 있는/ 없는 간선 DAG: 여러...","categories": ["Algorithm"],
+        "excerpt":"1. 그래프 설명 현실세계에서는 도로망, 지인관계, 링크관계 등의 연결고리들을 표현할 수 있다. 정의: 정점(vertex)과 간선(edge)의 집합 G(V,E) 정점: 노드(node)라고도 부른다. 간선: 유형에 따라 나뉜다. 두 정점을 이어주는 간선 자기자신을 이어주는 간선 방향이 있는 간선/ 없는 간선 - 방향이 있는 간선의 경우 사이클 발생 가능 가중치가 있는/ 없는 간선 DAG: 여러...","categories": ["Data Structure"],
         "tags": ["BFS","DFS","graph"],
-        "url": "http://localhost:4000/algorithm/graph/",
+        "url": "http://localhost:4000/data%20structure/graph/",
+        "teaser":null},{
+        "title": "Bfs",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/bfs/",
+        "teaser":null},{
+        "title": "DFS Algorithm",
+        "excerpt":"DFS(Depth First Search) - 깊이 우선 탐색 탐색 모든 정점들을 특정 순서에 따라 방문하는 알고리즘 그래프의 구조를 알 수 있다. (bfs, dfs 알고리즘을 이용) DFS는 그래프의 모든 정점을 발견하는 가장 단순하고 고전적인 방법 DFS 현재 정점에서 더 이상 다른 정점으로 이동할 수 없는 상태에 도달할 때까지 탐색 후 이전 정점으로...","categories": ["Algorithm"],
+        "tags": ["DFS","graph"],
+        "url": "http://localhost:4000/algorithm/dfs/",
         "teaser":null}]
